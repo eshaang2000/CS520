51189135f69eb87ff95e3bbaf358c0ac16ac7005
Eshaan Gandhi <eshaang2000@gmail.com>
2020-09-07 19:31:58 -0400

    switch

--- Maze/Grid.py
+++ Maze/Grid.py
@@ -1,17 +1,25 @@
 import numpy as np
+import random
+
 
 class Map:
-    def __init__(self, dim, p): #will know the prob of each maze
+    def __init__(self, dim):  # will know the prob of each maze
         self.dim = dim
-        self.p = p
+        # self.p = p
 
-        if p > 1 or p < 0:
-            raise ValueError("Probability must be between 0 and 1")
-        map1 = np.zeros((dim, dim))
-        print(map1)
+        # if p > 1 or p < 0:
+        #     raise ValueError("Probability must be between 0 and 1")
+        self.map1 = np.zeros((dim, dim))
+
+    def populate(self,p):
+        for i in self.map1:
+            for j in self.map1:
+                i=5
+                print(i)
+                print(j)
 
 
-m1 = Map(10, .5)
-
-    # def make:
-
+m1 = Map(10)
+m1.populate(0.5)
+print(m1.map1)
+# print(random.random())
