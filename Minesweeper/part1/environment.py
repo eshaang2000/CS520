b48555ff6622b0206b17bcd881cfefd42bcff54b
Eshaan Gandhi <eshaang2000@gmail.com>
2020-10-31 21:42:56 -0400

    .

--- Minesweeper/part1/environment.py
+++ Minesweeper/part1/environment.py
@@ -15,21 +15,14 @@ def makeGrid(n):
 
 # Function for setting up Mines
 def set_mines(numbers1, mines_no, n):
-    # global numbers
-    # global mines_no
-    # global n
-
     # Track of number of mines already set up
     count = 0
     while count < mines_no:
-
         # Random number from all possible grid positions
         val = random.randint(0, n * n - 1)
-
         # Generating row and column from the number
         r = val // n
         col = val % n
-
         # Place the mine, if it doesn't already have one
         if numbers1[r][col] != -1:
             count = count + 1
@@ -81,13 +74,6 @@ def printGrid(numbers1, n):
         print()
 
 
-def show_mines(numbers, n, mine_values):
-    for r in range(n):
-        for col in range(n):
-            if numbers[r][col] == -1:
-                mine_values[r][col] = 'M'
-
-
 def query(m, n, numbers):
     return numbers[m][n]
 
@@ -97,31 +83,3 @@ def makeBoard(n, mines):
     numbers = set_values(numbers, n)
     return numbers
 
-# printGrid(numbers, 8)
-# numbers = makeBoard(8, 8)
-# print(np.asarray(numbers))
-# print(query(6,4,numbers))
-
-
-# n = 8
-# Number of mines
-# mines_no = 8
-
-# The actual values of the grid
-# numbers = [[0 for y in range(n)] for x in range(n)]
-# print(numbers)
-# # The apparent values of the grid
-# mine_values = [[' ' for y in range(n)] for x in range(n)]
-# # The positions that have been flagged
-# flags = []
-#
-# # Set the mines
-# set_mines()
-# show_mines()
-#
-# # Set the values
-# set_values()
-# print(numbers)
-# print(mine_values)
-#
-# print_mines_layout()
