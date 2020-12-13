83ce2413cf54432302ebc7a49b97b739528fa611
Eshaan Gandhi <eshaang2000@gmail.com>
2020-12-13 18:13:33 -0500

    training works

--- Colorization/main.py
+++ Colorization/main.py
@@ -5,8 +5,7 @@ import random
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 
-
-path = "data.jpg"
+path = "data.png"
 image = Image.open(path)
 
 
@@ -43,7 +42,23 @@ def getInitialCentroids():  # returns one random centroid
     return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 
+def getInitialCentroidsBlack():
+    return (random.randint(0, 255), 255)
+
+
 def getKCentroids(k):
+    ans = []
+    i = 0
+    while i != k:
+        c1 = getInitialCentroids()
+        if c1 in ans:
+            continue
+        ans.append(c1)
+        i += 1
+    return ans
+
+
+def getKCentroidsBlack(k):
     ans = []
     i = 0
     while i != k:
@@ -69,6 +84,17 @@ def getDistance(p1, p2):
     return dist
 
 
+def getDistanceBlack(p1, p2):
+    x1 = p1[0]
+    x2 = p2[0]
+    y1 = p1[1]
+    y2 = p2[1]
+
+    dist = math.sqrt(math.pow(x2 - x1, 2) +
+                     math.pow(y2 - y1, 2) * 1.0)
+    return dist
+
+
 def average(d1, data):
     n = len(d1)
     a1 = 0
@@ -88,6 +114,21 @@ def average(d1, data):
     return t
 
 
+def averageBlack(d1, data):
+    n = len(d1)
+    a1 = 0
+    a2 = 0
+    for i in d1:
+        a1 += data[i[0]][i[1]][0]
+        a2 += data[i[0]][i[1]][1]
+    a1 = a1 / n
+    a2 = a2 / n
+    a1 = round(a1)
+    a2 = round(a2)
+    t = [a1, a2]
+    return t
+
+
 def classify(C, dataPoint):
     # find the centroid that is of minimum distance for that data point. C is a list of centroids that have to be
     # gone through
@@ -102,14 +143,28 @@ def classify(C, dataPoint):
     return minim
 
 
+def classifyBlack(C, dataPoint):
+    # find the centroid that is of minimum distance for that data point. C is a list of centroids that have to be
+    # gone through
+    minim = 100000000
+    minDist = 10000000
+    for j in range(len(C)):
+        dist = getDistanceBlack(dataPoint, C[j])
+        if minDist > dist:
+            minim = j
+            minDist = dist
+
+    return minim
+
+
 def saveImageFromArray(arr, mode):
     if mode == 1:
         img = Image.fromarray(arr.astype('uint8'), 'RGB')
-        img.save('my.jpg')
+        img.save('my.png')
     else:
         print(len(arr[0][0]))
         img = Image.fromarray(arr.astype('uint8'), 'L')
-        img.save('my.jpg')
+        img.save('my.png')
 
 
 #
@@ -149,11 +204,41 @@ def kMeansClustering(k, data):
     return image1, C, C1
 
 
+def kMeansClusteringBlack(k, data):
+    C = getKCentroidsBlack(k)
+    iter = 0
+    while iter != 8:
+        print("starting iter no " + str(iter))
+        C1 = []
+        for i in range(k):
+            C1.append(set())
+        for j in range(len(data)):
+            for i in range(len(data[0])):
+                index = classifyBlack(C, data[j][i])
+                # print(index)
+                C1[index].add((j, i))
+        for i in range(len(C1)):
+            if len(C1[i]) == 0:
+                continue
+            ctemp = averageBlack(C1[i], data)
+            C[i] = ctemp
+        # print(C)
+        iter += 1
+    # print(C)
+    image1 = np.zeros((len(data), len(data[0]), 2))
+    l = 0
+    for i in C1:
+        for j in i:
+            image1[j[0]][j[1]] = C[l]
+        l += 1
+    return image1, C, C1
+
+
 # make a matrix of averages of the gray scale array
 def grayAverage(grayMatrix):
     avg = np.zeros((len(grayMatrix), len(grayMatrix[0]), 2))
-    for i in range(1, len(grayMatrix)-1):
-        for j in range(1, len(grayMatrix[0])-1):
+    for i in range(1, len(grayMatrix) - 1):
+        for j in range(1, len(grayMatrix[0]) - 1):
             a = grayMatrix[i][j][0]
             a += grayMatrix[i + 1][j][0]
             a += grayMatrix[i - 1][j][0]
@@ -169,6 +254,14 @@ def grayAverage(grayMatrix):
     return avg
 
 
+def stripGray(grayData):
+    dope = np.zeros((len(grayData), len(grayData[0])))
+    dope.astype(np.uint8)
+    for i in range(len(grayData)):
+        for j in range(len(grayData[0])):
+            dope[i][j] = grayData[i][j][0]
+    return dope
+
 
 """
 1. Write code to get a single number that would get the rep of the 3x3 data
@@ -182,23 +275,50 @@ trainAvgGray = grayAverage(trainGray)
 # print(trainAvgGray)
 # df = pd.DataFrame(trainAvgGray)
 # print(df)
-print(trainAvgGray)
+# print(trainAvgGray)
+# ima = getArray(i)
+# ima, C, C1 = kMeansClusteringBlack(20, trainGray)
+# print(ima)
+# print(ima.shape)
+# ima2 = np.zeros((660, 495))
+# ima2.astype(np.uint8)
+# for i in range(len(ima)):
+#     for j in range(len(ima[0])):
+#         ima2[i][j] = ima[i][j][0]
+# print(ima2)
+# print(ima2.shape)
+# arr = np.random.randint(0,256, 100*100)
+# arr.resize((100,100))
+# arr.astype(np.uint8)
+# ima2 = trainGray
+# saveImageFromArray(trainRGB, 1)
+# da = Image.fromarray(ima.astype(np.uint8), mode='LA')
+# da.save("pls.png")
+
+
+
 
 temp = []
-for i in trainAvgGray:
-    for j in i:
-        temp.append(j)
+tempIndex = dict()
+for i in range(len(trainAvgGray)):
+    for j in range(len(trainAvgGray[0])):
+        temp.append(trainAvgGray[i][j])
+        tempIndex[len(temp)-1] = (i, j)
+
+
 temp1 = np.asarray(temp)
-for i in [291183, 185444, 261385, 313990, 204820]:
-    print(temp1[i])
-# nn = NearestNeighbors(5, algorithm='kd_tree')
-# k = nn.fit(temp1)
-# test = np.array([234, 255])
-# print("here")
-# print(nn.kneighbors(test.reshape(1, -1), 5))
-# print("now here")
+print(tempIndex)
+nn = NearestNeighbors(5, algorithm='kd_tree')
+k = nn.fit(temp1)
+test = np.array([234, 255])
+print("here")
+print(nn.kneighbors(test.reshape(1, -1), 5))
+print("now here")
 # imag, C, C1 = kMeansClustering(10, trainRGB)
-# trainG = np.zeros((len(trainGray), len(trainGray[0]), 1))
+
+# for i in [291183, 185444, 261385, 313990, 204820]:
+#     print(temp1[i])
+# # trainG = np.zeros((len(trainGray), len(trainGray[0]), 1))
 # for i in range(len(trainGray)):
 #     for j in range(len(trainGray[0])):
 #         trainG[i][j] = g[i][j][0]
@@ -207,4 +327,4 @@ for i in [291183, 185444, 261385, 313990, 204820]:
 # df = pd.DataFrame(g.reshape(len(g), len(g[0])))
 # df = pd.DataFrame(g.reshape(2, 2))
 ## save to xlsx file
-#[[291183, 185444, 261385, 313990, 204820]]
+# [[291183, 185444, 261385, 313990, 204820]]
